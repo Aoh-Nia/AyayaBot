7f54f00a875c88a0b612fc3bde66c405682eb5ca
Aoh-Nia <ainhoa.genshin@gmail.com>
2024-12-13 01:02:43 +0100

    Added Stuff to help with server

--- ayayabot.py
+++ ayayabot.py
@@ -2,6 +2,21 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 import os
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is alive!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
 
 # Define your bot's intents and command prefix
 intents = discord.Intents.default()
